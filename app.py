ca0a8c59d46ab865c05d1cbde8f3b113049d7a94
ghoshruby <60173326+ghoshruby@users.noreply.github.com>
2020-07-19 00:41:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,8 @@ Created on Sat Jul 18 21:37:08 2020
 """
 
 
-from flask import Flask, request
+from flask import Flask
 app = Flask(__name__)
-
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    
-    name = request.args.get("name", None)
-
 @app.route('/')
 def index():
     return "<h1>Hello World !!</h1>"
