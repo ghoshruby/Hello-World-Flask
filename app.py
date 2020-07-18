b78a665b6e8ffb557520abb9ea362885ab3f7ea8
ghoshruby <60173326+ghoshruby@users.noreply.github.com>
2020-07-19 00:07:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,18 +19,7 @@ def respond():
 
     response = {}
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
+    
 
 @app.route('/post/', methods=['POST'])
 def post_something():
