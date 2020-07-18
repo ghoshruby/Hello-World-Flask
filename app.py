67c7fc9819ea1b209d11db104a396079ba1d8903
ghoshruby <60173326+ghoshruby@users.noreply.github.com>
2020-07-19 00:11:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,21 +21,7 @@ def respond():
 
     
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+
 
 # A welcome message to test our server
 @app.route('/')
